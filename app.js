c5a5391c7a8cffdc1f7593fa248f9ddf2f2d0134
Vladyslav <dozdanikdozdzanik@gmail.com>
2026-03-19 13:10:53 +0200

    add herose_table.hbs and heroes routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import logger from 'morgan'
 import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
 import { fileURLToPath } from 'url';
+import weaponRouter from './routes/heroes_mlbb.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
